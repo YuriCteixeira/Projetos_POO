8b1cec1d616f80b9df5e51362f5612ed5974637d
yurict62@gmail.com <yuri.c@gmail.com>
2023-05-17 13:59:26 -0300

    Projeto Banco Brasil

---
+++ BancoBrasil/src/bancobrasil/BancoBrasil.java
@@ -0,0 +1,41 @@
+/*Yuri Carvalho Teixeira*/
+package bancobrasil;
+
+import java.util.Scanner;
+public class BancoBrasil {
+
+    
+    public static void main(String[] args) {
+       //Instanciando um objeto tipo ContaBancaria e referenciando na variavel contabancaria1//
+        Conta_bancaria contaBancaria1= new Conta_bancaria ();
+       Conta_bancaria contaBancaria2= new Conta_bancaria ();
+        /* Uso nas versões mais atuais da jdk
+       var contaBancaria1 = new Conta_bancaria();
+       
+        
+        //Atribuindo valores ao objeto//
+        contaBancaria1.agencia = "0001";
+        contaBancaria1.conta = "12345";
+        contaBancaria1.proprietario = "Junior";
+        contaBancaria1.saldo = 1.99;
+        
+        System.out.println(contaBancaria1.agencia+"\n"
+             + contaBancaria1.conta +"\n"
+             + contaBancaria1.proprietario +"\n"
+             + contaBancaria1.saldo);
+        */
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bem-vindo(a) ao Banco do Brasil");
+        System.out.println("Cadastro de Contas");
+        System.out.println("Digite a agência:");
+        contaBancaria1.agencia = input.next();
+        System.out.println("Digite a conta:");
+        contaBancaria1.conta = input.next();
+        System.out.println("Digite o proprietário:");
+        contaBancaria1.proprietario = input.next();
+        System.out.println("Digite o saldo inicial:");
+        contaBancaria1.saldo = input.nextDouble();
+     }
+    
+}
---
+++ BancoBrasil/src/bancobrasil/Conta_bancaria.java
@@ -0,0 +1,24 @@
+package bancobrasil;
+
+public class Conta_bancaria {
+
+    //atributos da classe//
+    String agencia;
+    String conta;
+    double saldo;
+    String proprietario;
+
+    //métodos da
+    void depositar() {
+    }
+
+    void sacar() {
+    }
+
+    void transferir() {
+    }
+
+    String consultar_saldo() {
+        return "";
+    }
+}
